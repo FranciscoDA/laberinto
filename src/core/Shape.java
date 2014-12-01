df9db61c711a59e7443a26e68d909f514486a0c5
FranciscoDA <franciscoda@outlook.com>
2014-12-01 19:29:13 -0300

    Eliminados los archivos residuales

--- src/core/Shape.java
+++
@@ -1,5 +0,0 @@
-package core;
-
-public interface Shape {
-	public boolean collides(Shape other);
-}
--- src/game/core/Moveable.java
+++
@@ -1,7 +0,0 @@
-package game.core;
-
-import state.GameState;
-
-public interface Moveable {
-	public void move (GameState gs);	
-}
